3ac0beb155ee7051bb29a08707686fe9af6af1f2
Andry Canel <andrybcanel@gmail.com>
2019-10-26 08:56:03 -0500

    started to create class for mobile goals

--- main.cpp
+++ main.cpp
@@ -1,11 +1,33 @@
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 
+class MBGoal {
+
+    public:
+
+    MBGoal(string, int, int);
+
+    int posX ,posY;
+    string name;
+
+
+};
+
+MBGoal:: MBGoal (string label,int a,int b){
+name =  label;
+   posX = a;
+   posY = b;
+
+}
 
 int main(){
-    cout << "Hello World!" << endl;
+
+
+    MBGoal mb1 ("mb1", 20 ,40);
+    cout << "Current position of "<< mb1.name<< " is "  << mb1.posX << ", " << mb1.posY << endl;
     return 0;
 }
 
