f91f93c2907dde2ba75fd4ecebe7b07f646c1d96
Andry Canel <andrybcanel@gmail.com>
2019-10-30 11:42:26 -0500

    dynamically create the mbl goals

--- main.cpp
+++ main.cpp
@@ -1,34 +1,83 @@
-
+#include<conio.h>
 #include <iostream>
 #include <string>
 using namespace std;
 
 
 class MBGoal {
-
+    int posX, posY;
+    string name;
+    bool isScored;
     public:
 
-    MBGoal(string, int, int);
+    //MBGoal(string, int, int);
 
-    int posX ,posY;
-    string name;
+    void setXY (int x, int y){
+        posX = x;
+        posY = y;
+    }
+    int getPosX () { return posX;}
+    int getPosY () { return posY;}
+    void changeName(string x){
+        name = x;
+    }
+    string getName(){
+
+        return name;
+    }
+
+
+
 
 
 };
-
-MBGoal:: MBGoal (string label,int a,int b){
+///constructor
+/*MBGoal:: MBGoal (string label,int a,int b){
 name =  label;
    posX = a;
    posY = b;
 
 }
-
+*/
 int main(){
 
+ MBGoal *MBG;
 
-    MBGoal mb1 ("mb1", 20 ,40);
-    cout << "Current position of "<< mb1.name<< " is "  << mb1.posX << ", " << mb1.posY << endl;
-    return 0;
+   int numMB;
+ //  clrscr();
+     cout<<"How many mobile Goals will there be?"<< endl;
+     cin >> numMB;
+  MBG = new MBGoal[numMB];
+
+
+     for (int i=0; i < numMB; i++){
+         //MBGoal a ("null", 0,0)
+         string name;
+         int positionX;
+         int positionY;
+         cout<<"Name of mobile goal #"<<(i+1)<<" : ";
+         cin>>name;
+         MBG[i].changeName(name);
+         cout << "Starting x position of "<< MBG[i].getName()<< " : ";
+         cin>> positionX;
+         cout << "Starting y position of "<< MBG[i].getName()<< " : ";
+         cin>> positionY;
+
+         MBG[i].setXY(positionX,positionY);
+
+     }
+
+
+   
+
+
+    /*MBGoal mb1 ("mb1", 20 ,40);
+
+    ptr = &mb1;
+
+    cout << "Current position of "<< ptr->getName()<< " is "  << ptr->getPosX()<< ", " << ptr->getPosX()<< endl;
+   
+   */ return 0;
 }
 
 
